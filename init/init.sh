ac139ee9a44a7f3ee5f2807b011e9574114666d1
cyan21 <yann.chaysinh@gmail.com>
2022-10-17 17:45:56 +0200

    do not display the password if the ci user already existed

--- init/init.sh
+++ init/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 server_id="jfrog-platform-training"
+ci_created=false 
 
 # install CLI in the current folder
 curl -fL -s https://getcli.jfrog.io/v2-jf | sh
@@ -26,8 +27,11 @@ fi
 password="`echo $RANDOM | shasum | cut -d" " -f1`L@Z"
 ./jf rt user-create ci "${password}" "robot.doe@nobody.org" --server-id $server_id
 
-#### Create Permission Targets
-./jf rt ptu rt/ci_permissions.json --server-id $server_id
+if [[ $? -eq 0 ]]; then 
+    #### Create Permission Targets
+    ./jf rt ptu rt/ci_permissions.json --server-id $server_id
+    ci_created=true 
+fi
 
 # XRAY - CONFIG
 ##### Create policies 
@@ -43,17 +47,19 @@ for w in `ls xr/watch*`; do
     ./jf xr curl -H "Content-Type: application/json" -XPOST api/v2/watches -T $w --server-id $server_id
 done
 
-Configuring GHA requires to create 2 secrets : 
-A/ configure CLI
-B/ Docker registry
+# Configuring GHA requires to create 2 secrets : 
+# A/ configure CLI
+# B/ Docker registry
 
 # A/ show the token for GHA
 jf_secret=`./jf c export $server_id`
 
 echo "*********************************"
-echo "In Github, create these 2 secrets :"
+echo "Use these secret(s) to run your Github Workflows :"
 echo -e "\t  - JF_SECRET : $jf_secret"
 
-# B/ show the ci user's password
-echo -e "\t  - JF_CI_SECRET : $password"
+# B/ show the ci user's password only if he was created
+if [[ $ci_created == "true" ]]; then 
+    echo -e "\t  - JF_CI_SECRET : $password"
+fi
 echo "*********************************"
